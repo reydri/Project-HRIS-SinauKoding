e818dfda2b599965521c59f343ed758e68e28119
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-12-22 12:55:59 +0700

    update dao attendance

--- src/main/java/com/sinaukoding/absence/dao/AttendanceDAO.java
+++ src/main/java/com/sinaukoding/absence/dao/AttendanceDAO.java
@@ -4,6 +4,7 @@ import com.sinaukoding.absence.entity.Attendance;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
@@ -13,6 +14,10 @@ public class AttendanceDAO extends BaseDAO<Attendance> {
     @Override
     public List<Predicate> predicates(Attendance param, CriteriaBuilder builder, Root<Attendance> root, boolean isCount){
         List<Predicate> predicates = super.predicates(param, builder, root,isCount);
+
+        if(!isCount){
+            root.fetch("employee", JoinType.INNER);
+        }
 
         return predicates;
     }
