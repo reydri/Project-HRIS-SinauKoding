9b00004f410c67c32986e78bc7c5af98984dcf5e
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-12-22 12:57:20 +0700

    update service employee

--- src/main/java/com/sinaukoding/absence/service/EmployeeService.java
+++ src/main/java/com/sinaukoding/absence/service/EmployeeService.java
@@ -5,6 +5,9 @@ import com.sinaukoding.absence.dao.BaseDAO;
 import com.sinaukoding.absence.dao.EmployeeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 
 @Service
@@ -16,5 +19,30 @@ public class EmployeeService extends BaseService<Employee> {
     @Override
     protected BaseDAO<Employee> getDAO() {
         return dao;
+    }
+
+    @Transactional
+    public Employee save(Employee entity){
+        entity.setStartDate(new Date());
+
+        return dao.save(entity);
+    }
+
+    @Transactional
+    public Employee update(Employee entity) {
+        if (entity.getId() != null) {
+            Employee reference = getDAO().findReference(entity.getId());
+
+            reference.setEndDate(entity.getEndDate() != null
+                    ? entity.getEndDate()
+                    : new Date());
+
+            entity.setStartDate(reference.getStartDate());
+            entity.setEndDate(reference.getEndDate());
+
+            return entity;
+        }
+
+        return null;
     }
 }
