62a259296cab0b58836e59b5e02eb257b7acde7f
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-12-22 15:25:52 +0700

    implementation register and login for authentication

--- src/main/java/com/sinaukoding/absence/controller/AuthController.java
+++ src/main/java/com/sinaukoding/absence/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sinaukoding.absence.controller;
 
+import com.sinaukoding.absence.common.RestResult;
 import com.sinaukoding.absence.entity.User;
 import com.sinaukoding.absence.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,4 +14,14 @@ public class AuthController extends BaseController {
 
     @Autowired
     private UserService service;
+
+    @PostMapping(value = "do-login")
+    public RestResult doLogin(@RequestBody User user){
+        return service.login(user);
+    }
+
+    @PostMapping(value = "do-register")
+    public RestResult register(@RequestBody User param){
+        return new RestResult(service.register(param, User.Role.ROLE_USER));
+    }
 }
