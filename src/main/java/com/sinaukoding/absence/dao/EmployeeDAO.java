a4908b13ef9b32064f090ddbbbe8a5eec03cce31
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-12-23 12:44:20 +0700

    custom dao employee nip, status and fix get employee user iscount

--- src/main/java/com/sinaukoding/absence/dao/EmployeeDAO.java
+++ src/main/java/com/sinaukoding/absence/dao/EmployeeDAO.java
@@ -4,6 +4,7 @@ import com.sinaukoding.absence.entity.Employee;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
@@ -13,6 +14,22 @@ public class EmployeeDAO extends BaseDAO<Employee> {
     @Override
     public List<Predicate> predicates(Employee param, CriteriaBuilder builder, Root<Employee> root, boolean isCount){
         List<Predicate> predicates = super.predicates(param, builder, root,isCount);
+
+        if (param != null){
+            if (param.getStatus() != null){
+                predicates.add(builder.equal(root.get("status"), param.getStatus()));
+            }
+        }
+
+        if (param != null){
+            if (param.getNip() != null){
+                predicates.add(builder.equal(root.get("nip"), param.getNip()));
+            }
+        }
+
+        if (!isCount){
+            root.fetch("user", JoinType.INNER);
+        }
 
         return predicates;
     }
