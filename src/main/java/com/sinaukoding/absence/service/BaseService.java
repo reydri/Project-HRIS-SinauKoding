83cc34114365f028acc84f71c69879e395838354
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-12-27 15:02:33 +0700

    update base entity with add searchbyid and statusdelete

--- src/main/java/com/sinaukoding/absence/service/BaseService.java
+++ src/main/java/com/sinaukoding/absence/service/BaseService.java
@@ -1,10 +1,12 @@
 package com.sinaukoding.absence.service;
 
+import com.sinaukoding.absence.AbsenceApplication;
 import com.sinaukoding.absence.entity.BaseEntity;
 import com.sinaukoding.absence.dao.BaseDAO;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.Date;
 
 public abstract class BaseService<T extends BaseEntity<T>> {
 
@@ -41,5 +43,25 @@ public abstract class BaseService<T extends BaseEntity<T>> {
     @Transactional
     public boolean delete(Long id){
         return getDAO().delete(getDAO().findReference(id)) != null;
+    }
+
+    @Transactional
+    public T searchById(Long id){
+        return getDAO().findReference(id);
+    }
+
+    @Transactional
+    public void statusDelete(Long id){
+        T reference = getDAO().findReference(id);
+
+        if (reference != null){
+            reference.setDeletedTime(reference.getDeletedTime() != null
+                    ? reference.getDeletedTime()
+                    : new Date());
+
+            reference.setDeletedBy(reference.getDeletedBy() != null
+                    ? reference.getDeletedBy()
+                    : AbsenceApplication.getCurrentUser().getId());
+        }
     }
 }
